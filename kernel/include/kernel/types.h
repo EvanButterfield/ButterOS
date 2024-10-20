fc14ffda61c369d850d7637a454a7ca7d8e78d74
Evan Butterfield <evan.e.butterfield@gmail.com>
2024-10-20 14:25:44 -0700

    Basic Types

---
+++ kernel/include/kernel/types.h
@@ -0,0 +1,17 @@
+#ifndef BUTTER_KERNEL_TYPES
+#define BUTTER_KERNEL_TYPES
+
+typedef UINT8  u8;
+typedef UINT16 u16;
+typedef UINT32 u32;
+typedef UINT64 u64;
+typedef UINTN  un;
+
+typedef INT8  s8;
+typedef INT16 s16;
+typedef INT32 s32;
+typedef INT64 s64;
+
+typedef s32 b32;
+
+#endif // BUTTER_KERNEL_TYPES
